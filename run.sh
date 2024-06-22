f295b7ddd6d230b169ef472837b70989aa1e49d7
Wagner Santos <prog.wagner@gmail.com>
2024-06-22 19:58:37 -0300

    edit run for ios

--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# Function to display usage
+usage() {
+    echo "Usage: $0 [--ios]"
+    exit 1
+}
+
+# Parse command line arguments
+TARGET="default"
+while [ "$1" != "" ]; do
+    case $1 in
+        --ios )  TARGET="ios"
+                 ;;
+        * )      usage
+                 exit 1
+    esac
+    shift
+done
+
 # Check if poetry is installed
 if ! command -v poetry &> /dev/null
 then
@@ -11,6 +29,11 @@ fi
 echo "Installing dependencies..."
 poetry install
 
-# Run the Flet app with hot reload
-echo "Running the Flet app with hot reload..."
-poetry run flet run -d -r main.py
+# Run the Flet app
+if [ "$TARGET" = "ios" ]; then
+    echo "Running the Flet app on iOS..."
+    poetry run flet run -d -r --ios main.py
+else
+    echo "Running the Flet app with hot reload..."
+    poetry run flet run -d -r main.py
+fi
